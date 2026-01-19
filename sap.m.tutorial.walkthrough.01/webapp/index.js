d5871867802560f79384c92530897f789df99026
AlissCava <cavalleraalissa@gmail.com>
2026-01-19 15:24:05 +0100

    Step 3: Controls

--- sap.m.tutorial.walkthrough.01/webapp/index.js
+++ sap.m.tutorial.walkthrough.01/webapp/index.js
@@ -1,18 +1,18 @@
-sap.ui.define([], () => {
+sap.ui.define([
+	"sap/m/Text"
+], (Text) => {
 	"use strict";
-	alert("UI5 is ready");
+
+	new Text({
+		text: "Hello World"
+	}).placeAt("content");
 });
 
 /*
-Questo file JavaScript è il modulo di avvio dell’applicazione SAP UI5.
-
-Viene caricato automaticamente dal bootstrap UI5 tramite
-l’attributo data-sap-ui-on-init definito nel file HTML.
-
-sap.ui.define definisce un modulo UI5 (senza dipendenze in questo caso)
-e garantisce che il codice venga eseguito solo quando il framework
-è completamente inizializzato.
-
-L’alert serve solo come verifica che UI5 sia stato caricato correttamente
-e che l’applicazione sia pronta.
+Modulo UI5:
+- importa il modulo sap.m.Text come dipendenza
+- definisce la funzione di callback in modalità "use strict"
+- crea una nuova istanza del controllo Text
+- assegna la stringa "Hello World" alla proprietà text
+- inietta il controllo nell'elemento del DOM con ID "content"
 */
