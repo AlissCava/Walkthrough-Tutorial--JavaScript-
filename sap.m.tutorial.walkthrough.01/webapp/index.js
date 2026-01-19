8798663d425bc1c11f24eb3e3a60212bdc6b18c9
AlissCava <cavalleraalissa@gmail.com>
2026-01-19 14:44:57 +0100

    Step 2: Bootstrap

---
+++ sap.m.tutorial.walkthrough.01/webapp/index.js
@@ -0,0 +1,18 @@
+sap.ui.define([], () => {
+	"use strict";
+	alert("UI5 is ready");
+});
+
+/*
+Questo file JavaScript è il modulo di avvio dell’applicazione SAP UI5.
+
+Viene caricato automaticamente dal bootstrap UI5 tramite
+l’attributo data-sap-ui-on-init definito nel file HTML.
+
+sap.ui.define definisce un modulo UI5 (senza dipendenze in questo caso)
+e garantisce che il codice venga eseguito solo quando il framework
+è completamente inizializzato.
+
+L’alert serve solo come verifica che UI5 sia stato caricato correttamente
+e che l’applicazione sia pronta.
+*/
