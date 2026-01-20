af866ad40519b7eaa7f6d98b97e2d0de88a15cfa
AlissCava <cavalleraalissa@gmail.com>
2026-01-20 11:38:10 +0100

    Step 6: Modules

--- sap.m.tutorial.walkthrough.01/webapp/controller/App.controller.js
+++ sap.m.tutorial.walkthrough.01/webapp/controller/App.controller.js
@@ -1,21 +1,22 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
 ], (Controller) => {
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
       onShowHello() {
          // show a native JavaScript alert
-         alert("Hello World");
+         MessaggeToast.show("Hello World");
       }
    });
 });
 
 /*
-Implementazione Controller (App.controller.js):
-- importa il modulo base sap.ui.core.mvc.Controller
-- estende la classe Controller per definire la logica di business della vista
-- adotta il namespace "ui5.walkthrough.controller.App" per garantire l'unicità
-- definisce l'event handler "onShowHello" seguendo la convenzione del prefisso "on"
-- implementa un'azione di feedback immediato tramite un alert JavaScript nativo
+Evoluzione Controller con MessageToast (App.controller.js):
+- estende le dipendenze includendo il modulo "sap/m/MessageToast"
+- sfrutta la sintassi AMD (Asynchronous Module Definition) per separare il caricamento dei moduli dall'esecuzione, migliorando le performance
+- mappa i moduli caricati su parametri della funzione callback (es. MessageToast) per un accesso semplificato senza namespace
+- utilizza "sap.ui.define" per dichiarare un namespace globale che renda il controller rintracciabile nell'app
+- sostituisce l'alert nativo con "MessageToast.show", offrendo un feedback utente moderno e non bloccante
 */
